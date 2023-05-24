9ee00d51b40a10416944235e346ae92ff8d7c846
Nick <nicky.kravtsov@gmail.com>
2023-05-24 13:13:11 +0300

    added first widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,20 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles/index.scss';
 import {useTheme} from "app/providers/ThemeProviders";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+
 import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
         </div>
     );
 };
