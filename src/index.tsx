511cecfe63d4bf3f8654581462fa2a1daea4e65b
Nick <nicky.kravtsov@gmail.com>
2023-05-26 15:18:12 +0300

    added i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import {render} from "react-dom";
-import React from "react";
 import App from "./app/App";
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "app/providers/ThemeProviders";
+
+import "./shared/config/i18n/i18n";
 
 render(
     <BrowserRouter>
