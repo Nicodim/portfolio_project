75b9d3e2895d9b6cd5753a37fe283989b844a767
Nick <nicky.kravtsov@gmail.com>
2023-06-05 15:59:39 +0300

    added all components to storybook

---
+++ config/storybook/preview.ts
@@ -0,0 +1,18 @@
+import { Preview } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+
+const preview: Preview = {
+    decorators: [
+        StyleDecorator,
+        ThemeDecorator(Theme.LIGHT),
+        RouterDecorator,
+    ],
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+    },
+};
+
+export default preview;
