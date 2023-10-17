9abf9f0b455c3d9f2267ef20853fc5bc0b481961
Nick <nicky.kravtsov@gmail.com>
2023-10-17 13:30:25 +0300

    small fixes in storybook config

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,4 +1,9 @@
+import path from 'path';
+
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import { Configuration, DefinePlugin, RuleSetRule } from 'webpack';
+
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
 
 const config: StorybookConfig = {
     stories: [
@@ -20,8 +25,44 @@ const config: StorybookConfig = {
         name: '@storybook/react-webpack5',
         options: {},
     },
-    docs: {
-        autodocs: 'tag',
+    webpackFinal: async (config: Configuration) => {
+        const paths = {
+            build: '',
+            html: '',
+            entry: '',
+            src: path.resolve(__dirname, '..', '..', 'src'),
+            locales: '',
+            buildLocales: '',
+        };
+        config!.resolve!.modules!.push(paths.src);
+        config!.resolve!.extensions!.push('.ts', '.tsx');
+        config!.resolve!.alias = {
+            ...config!.resolve!.alias,
+            '@': paths.src,
+        };
+
+        // @ts-ignore
+        config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
+            if (/svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
+
+            return rule;
+        });
+
+        config!.module!.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        config!.module!.rules.push(buildCssLoader(true));
+
+        config!.plugins!.push(new DefinePlugin({
+            __IS_DEV__: JSON.stringify(true),
+            __API__: JSON.stringify('https://testapi.ru'),
+            __PROJECT__: JSON.stringify('storybook'),
+        }));
+        // Return the altered config
+        return config;
     },
 };
 export default config;
