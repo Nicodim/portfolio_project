0ecf99277d5e45a642f1208452c3ed201a5b818a
Nick <nicky.kravtsov@gmail.com>
2023-10-24 15:21:21 +0300

    added feature flag concept

---
+++ src/shared/lib/features/setGetFeatures.ts
@@ -0,0 +1,14 @@
+import { FeatureFlags } from '@/shared/types/featureFlags';
+
+// FEATURES DO NOT CHANGE DURING THE SESSION, THEY DO NOT HAVE TO BE MADE REACTIVE!
+let featureFlags: FeatureFlags;
+
+export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
+    if (newFeatureFlags) {
+        featureFlags = newFeatureFlags;
+    }
+}
+
+export function getFeatureFlag(flag: keyof FeatureFlags) {
+    return featureFlags[flag];
+}
