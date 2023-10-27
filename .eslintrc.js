83db16227d2399c8c9d0920890497a582aa8a351
Nick <nicky.kravtsov@gmail.com>
2023-10-27 17:11:10 +0300

    added toggleFeatures for components

--- .eslintrc.js
+++ .eslintrc.js
@@ -64,6 +64,7 @@ module.exports = {
                     'direction',
                     'gap',
                     'border',
+                    'feature',
                 ],
             },
         ],
