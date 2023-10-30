1eb9a94b7686981d4aad511ae24d98d27ddb3c29
Nick <nicky.kravtsov@gmail.com>
2023-10-30 16:45:18 +0200

    mark all shared components as deprecated

---
+++ src/shared/ui/deprecated/Skeleton/Skeleton.tsx
@@ -0,0 +1,32 @@
+import { CSSProperties, memo } from 'react';
+
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+import cls from './Skeleton.module.scss';
+
+export interface SkeletonProps {
+    className?: string;
+    height?: number | string;
+    width?: number | string;
+    border?: string;
+}
+
+/**
+ * Устарел, используем новые компоненты из папки redesigned
+ * @deprecated
+ */
+export const Skeleton = memo((props: SkeletonProps) => {
+    const { className, border, height, width } = props;
+
+    const styles: CSSProperties = {
+        width,
+        height,
+        borderRadius: border,
+    };
+    return (
+        <div
+            className={classNames(cls.Skeleton, {}, [className])}
+            style={styles}
+        />
+    );
+});
