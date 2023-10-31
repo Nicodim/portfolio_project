a13c2d41835835c2182b1410cf0a88fbb47ead2a
Nick <nicky.kravtsov@gmail.com>
2023-10-31 16:20:21 +0200

    redesigned sidebar 2.0

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -39,8 +39,8 @@ const config: StorybookConfig = {
             '@': paths.src,
         };
 
-        // @ts-ignore
         config!.module!.rules = config!.module!.rules!.map(
+            // @ts-ignore
             (rule: RuleSetRule) => {
                 if (/svg/.test(rule.test as string)) {
                     return { ...rule, exclude: /\.svg$/i };
