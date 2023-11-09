e95b76f401e2c85ac0d396d69b4a7621781423d3
Nick <nicky.kravtsov@gmail.com>
2023-11-09 12:25:06 +0200

    added scrollTopToolbar widget, useAppToolbar useRoute change

---
+++ src/features/scrollToTopButton/ui/ScrollToTopButton/ScrollToTopButton.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ScrollToTopButton } from './ScrollToTopButton';
+// eslint-disable-next-line nicodim-plugin/layer-imports
+import 'app/styles/index.scss';
+
+const meta: Meta<typeof ScrollToTopButton> = {
+    title: 'features/ScrollToTopButton',
+    component: ScrollToTopButton,
+    tags: ['autodocs'],
+    argTypes: {
+        // @ts-ignore
+        backgroundColor: { control: 'color' },
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof ScrollToTopButton>;
+
+export const Primary: Story = {
+    args: {},
+};
--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -14,6 +14,7 @@ export enum AppRoutes {
 }
 
 export const getRouteMain = () => '/';
+export const getRouteSettings = () => '/settings';
 export const getRouteAbout = () => '/about';
 export const getRouteProfile = (id: string) => `/profile/${id}`;
 export const getRouteArticles = () => '/articles';
@@ -22,4 +23,16 @@ export const getRouteArticleCreate = () => '/articles/new';
 export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
 export const getRouteAdmin = () => '/admin';
 export const getRouteForbidden = () => '/forbidden';
-export const getRouteSettings = () => '/settings';
+
+export const AppRouteByPathPattern: Record<string, AppRoutes> = {
+    [getRouteMain()]: AppRoutes.MAIN,
+    [getRouteSettings()]: AppRoutes.SETTINGS,
+    [getRouteAbout()]: AppRoutes.ABOUT,
+    [getRouteProfile(':id')]: AppRoutes.PROFILE,
+    [getRouteArticles()]: AppRoutes.ARTICLES,
+    [getRouteArticleDetails(':id')]: AppRoutes.ARTICLE_DETAILS,
+    [getRouteArticleCreate()]: AppRoutes.ARTICLE_CREATE,
+    [getRouteArticleEdit(':id')]: AppRoutes.ARTICLE_EDIT,
+    [getRouteAdmin()]: AppRoutes.ADMIN_PANEL,
+    [getRouteForbidden()]: AppRoutes.FORBIDDEN,
+};
